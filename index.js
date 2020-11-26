ba06d8104e0a078d47a96bf8282773e8b6066af3
caqg95 <cquinterogrande@yahoo.es>
2020-11-25 21:47:27 -0600

    End Example Socket.io

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 var path= require('path');
+var express = require('express');
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public','index.html'));
-});
+app.use(express.static(path.join(__dirname,'public')));
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected',socket.id);
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+        console.log(msg);
         io.emit('chat message', msg);
     });
+    socket.on('chat typing', (msg) => {
+        console.log(msg);
+        socket.broadcast.emit('chat typing', msg);
+    });
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected',socket.id);
     });
 });
 
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,38 @@
-console.log('lol');
+const socket = io();
+
+
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+let btn = document.getElementById('send');
+
+message.addEventListener('keypress', () => {
+    socket.emit("chat typing", {
+        username: username.value
+    });
+});
+
+socket.on("chat typing", (data) => {
+    actions.innerHTML = `
+                        <p>
+                            <em>${data.username} is typing a message</em>: 
+                        </p>
+                    `
+});
+
+btn.addEventListener('click', () => {
+    socket.emit("chat message", {
+        message: message.value,
+        username: username.value
+    });
+});
+socket.on("chat message", (data) => {
+    actions.innerHTML='';
+    output.innerHTML += `
+                        <p>
+                            <strong>${data.username}</strong>: ${data.message}
+                        </p>
+                    `
+});
